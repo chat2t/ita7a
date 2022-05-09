645938dcd6db9e9c84aa3c6d13025034982d6bad
chat2t <105184145+chat2t@users.noreply.github.com>
2022-05-09 05:28:15 +0300

    Create ita7a.js

---
+++ ita7a.js
@@ -0,0 +1,17 @@
+$('.hand.nosel.fl.uzr.border').css({'margin': '5px 5',borderRight: '8px solid #FFFFFF',borderLeft: '7px solid #FFFFFF',borderTop: '5px solid #FFFFFF',borderBottom: '2px solid #E9EEF7',paddingTop: '5px',borderRadius: '7px'});
+													
+
+$(`<img tabindex="0" class="al120 fl " style="margin-left: -4px; padding:5px; width:40px;margin-top: -4px;" src="/sico/1647914718509.png" data-original-title="" title="">`).insertAfter('img.fl.nosel.emobox');
+$(".al120").click(function(){
+alert("عذرا لا تملك الصلاحية للدخول  ");
+});
+$('.dpnl').append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><input type="Muhanad" name="FirstName" value="FM اذاعة  ">
+<audio class="wp-audio-shortcode" id="audio-236-1" preload="none" style="width: 100%;" controls="controls"><source type="audio/mpeg" src="http://curiosity.shoutca.st:6035/ ؛"><a href="</a></audio>
+<center></center></center>
+</div>`);
+$('#d0').append(`<label title="الاذاعه" href="#" onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},100);$('.dpnl').show();"
+data-toggle="tab" data-target="#mic"  class="ae fa label label-primary fa-microphone ">FM</label>`);
+$("#d0 .fa-gear").html('&nbsp;').css('width','36px');
+$(`<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5"><font style="background-color: #‏B7B6BA;border-radius: 16px 16px 16px 15px;font-family: 'jazeera',FontAwesome;padding: 5px 10px 5px 10px;margin: 10px 13px 10px 13px;"><img src="/sico/1651362452365.gif" style="width: 100%;transform:matrix(1, 0, 0, 1, 8, 0);"></a>
+<div style=" width: 100%; " id="ita7a" class="btn btn-primary btn/marquee></font></b></td>`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success'); 
+$('.break.light').css({"background-image":"url()"});
